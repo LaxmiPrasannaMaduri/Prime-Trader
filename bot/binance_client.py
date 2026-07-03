74d9d5cca78704e1c9c8a67d16755792e56b0008
LaxmiPrasannaMaduri <madurilaxmiprasanna123@gmail.com>
2026-07-03 13:37:04 +0530

    Add testnet URL for Binance futures client

--- bot/binance_client.py
+++ bot/binance_client.py
@@ -11,5 +11,7 @@ class BinanceFuturesClient:
         self.client = Client(self.api_key, self.api_secret)
         self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
 
+
+    
     def place_order(self, **kwargs):
         return self.client.futures_create_order(**kwargs)
